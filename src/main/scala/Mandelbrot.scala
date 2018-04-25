4919610528a717bd88f0aa1f19d8dce5e827abe3
gabrielevi10 <gabrielevigomes@gmail.com>
2018-04-25 14:59:50 -0300

    Minor refactor Now the class Julia is in a separated .scala file.

--- src/main/scala/Mandelbrot.scala
+++ src/main/scala/Mandelbrot.scala
@@ -41,22 +41,3 @@ class Mandelbrot {
 
   }
 }
-
-class Julia extends Mandelbrot {
-  override def converges(z: ComplexNumber, maxIt: Int): Double = {
-    var c = ComplexNumber(0.45, 0.1428)
-    var zz = z
-    var it = 0
-
-    while (zz.absSquare <= 4.0 && it < maxIt) {
-      zz = zz * zz + c
-      it += 1
-    }
-    if (it < maxIt) {
-      (it - log2(log2(z.abs))) / maxIt
-    }
-    else {
-      Double.PositiveInfinity
-    }
-  }
-}
