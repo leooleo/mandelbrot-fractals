ea54d630f0437ace2a2fb758d7063292da310049
LPignata <luanpignata@aluno.unb.br>
2018-05-08 23:17:13 -0300

    Adicionando scale no fractal mandelbrot

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,14 @@
 window.onload = function() {
     var elements = 0;
-    console.log(document.getElementById("fractal-1").offsetHeight );
     if (elements < document.getElementById("fractal-1").offsetHeight ) elements = document.getElementById("fractal-1").offsetHeight ;
     if (elements < document.getElementById("fractal-2").offsetHeight ) elements = document.getElementById("fractal-2").offsetHeight ;
     if (elements < document.getElementById("fractal-3").offsetHeight ) elements = document.getElementById("fractal-3").offsetHeight ;
     if (elements < document.getElementById("fractal-4").offsetHeight ) elements = document.getElementById("fractal-4").offsetHeight ;
     if (elements < document.getElementById("fractal-5").offsetHeight ) elements = document.getElementById("fractal-5").offsetHeight ;
 
-    document.getElementById("fractal-1").style.height = elements + "px";
-    document.getElementById("fractal-2").style.height = elements + "px";
-    document.getElementById("fractal-3").style.height = elements + "px";
-    document.getElementById("fractal-4").style.height = elements + "px";
-    document.getElementById("fractal-5").style.height = elements + "px";
-    console.log(elements);
+    document.getElementById("fractal-1").style.minHeight = elements + "px";
+    document.getElementById("fractal-2").style.minHeight = elements + "px";
+    document.getElementById("fractal-3").style.minHeight = elements + "px";
+    document.getElementById("fractal-4").style.minHeight = elements + "px";
+    document.getElementById("fractal-5").style.minHeight = elements + "px";
 }
