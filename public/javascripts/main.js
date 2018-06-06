6bfd814f984c23e414b2236b1a28da8295986133
leooleo <leo.ms097@gmail.com>
2018-06-06 11:20:50 -0300

    Add html

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,19 @@
-window.onload = function() {
-    var elements = 0;
-    if (elements < document.getElementById("fractal-1").offsetHeight ) elements = document.getElementById("fractal-1").offsetHeight ;
-    if (elements < document.getElementById("fractal-2").offsetHeight ) elements = document.getElementById("fractal-2").offsetHeight ;
-    if (elements < document.getElementById("fractal-3").offsetHeight ) elements = document.getElementById("fractal-3").offsetHeight ;
-    if (elements < document.getElementById("fractal-4").offsetHeight ) elements = document.getElementById("fractal-4").offsetHeight ;
-    if (elements < document.getElementById("fractal-5").offsetHeight ) elements = document.getElementById("fractal-5").offsetHeight ;
-
-    document.getElementById("fractal-1").style.minHeight = elements + "px";
-    document.getElementById("fractal-2").style.minHeight = elements + "px";
-    document.getElementById("fractal-3").style.minHeight = elements + "px";
-    document.getElementById("fractal-4").style.minHeight = elements + "px";
-    document.getElementById("fractal-5").style.minHeight = elements + "px";
+function divChangeHeight(div) {
+    $('#'+div).height($( window ).height());
 }
+
+function divsChange() {
+    for (var i = 1; i < 7; i++)
+        divChangeHeight('fractal-'+i);
+}
+
+$( document ).ready(function() {
+    divsChange();
+})
+
+$( window ).resize(function() {
+    divsChange();
+})
 
 $("#cantor").click(() => {
     $("#image").attr("src","assets/images/loading.gif");
